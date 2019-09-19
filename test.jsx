902550bd9ad5e7c679598eaa3336f4bc7bb91803
Roman <pod1987@gmail.com>
2019-09-19 23:23:24 +0700

    add templateScript

---
+++ test.jsx
@@ -0,0 +1,6 @@
+﻿//~ import { tmplt6006 }  from 'templateScripts/6006.jsx'
+#include '/d/Scripts/photoshop/templateScripts/6006.jsx'
+//~ tmplt6006()
+//require('templateScripts/6006.jsx');
+//~ var flScrip = new File ('/d/Scripts/photoshop/templateScripts/6006.jsx')
+//~ flScrip.execute()
