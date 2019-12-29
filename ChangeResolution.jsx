be4b60b17703c775f336224470044d81b8ff322a
Roman <pod1987@gmail.com>
2019-12-29 16:09:15 +0700

    Create ChangeResolution.jsx

---
+++ ChangeResolution.jsx
@@ -0,0 +1,25 @@
+﻿function typeDetect(tpName){
+    var listTpName = ["20x30","Настенный календарь"];
+    for(var i=0;i<listTpName.length;i++){
+            tpNameReg = new RegExp (listTpName[i]);
+            if(tpNameReg.test(tpName)){
+               return false; 
+               }
+        }
+        return true;
+}
+
+
+function changeResolution(){
+    app.activeDocument.resizeImage (undefined, undefined, 300);
+}
+
+function main(){
+    flName = app.activeDocument.name;
+    if (typeDetect (flName)){
+            changeResolution();
+        }
+    
+    }
+
+main()
